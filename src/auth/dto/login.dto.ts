a5e064a6b1b8938707cfc9cda1ce6e6b5fd72c1a
RiqueVaz <henriquevazctba@hotmail.com>
2025-06-23 01:02:16 -0300

    corrigindo estrutura

--- src/auth/dto/login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
-export class LoginDto {
-  @IsEmail({}, { message: 'E-mail inválido' })
-  email: string;
-
-  @IsNotEmpty({ message: 'Password não pode ser vazio' })
-  password: string;
-}
